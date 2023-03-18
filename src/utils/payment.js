ce8e6e7503a8c027f6f400354bf7b0b00f43cc2a
Yash <yashvadhvani@gmail.com>
2023-03-18 07:18:53 +0000

    backup

--- src/utils/payment.js
+++ src/utils/payment.js
@@ -3,7 +3,7 @@ export const CoinPayment = async ({ amount }) => {
   // const createTransaction = async () => {
 
   try {
-    const API_BASE_URL = "http://localhost:5001";
+        const API_BASE_URL = "https://apis.mazimatic.com";
 
     const response = await fetch(`${API_BASE_URL}/api/coinpayment-link`, {
       method: "POST",
