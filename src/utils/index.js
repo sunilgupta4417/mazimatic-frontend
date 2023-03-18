bc535063cd56c209d4d2d669a9cfc51b7bddb7bb
Yash <yashvadhvani@gmail.com>
2023-03-18 21:36:10 +0530

    fix: getprice fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 import { COIN_PRICE } from "../constants";
 
-export const getPrice = (type) =>
-  typeof parseInt(type) === "number" && type > 0
-    ? COIN_PRICE
-    : (COIN_PRICE * 20) / 100;
+export const getPrice = (type) => {
+  return typeof parseInt(type) === "number" && type > 0
+    ? `${COIN_PRICE - (COIN_PRICE * 20) / 100}0`
+    : COIN_PRICE;
+};
